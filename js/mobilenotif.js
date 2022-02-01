b28fa4c2bec589beefa13d9eb8a6c5325aed721c
JacobK <techsupport@kreindler.ca>
2022-02-01 18:42:00 -0500

    feat(mobilenotif): added popup on mobile devices letting people know the styling for mobile can be a bit funky

---
+++ js/mobilenotif.js
@@ -0,0 +1,3 @@
+if (screen.width <= 480) {
+    alert("Hello! Just a frendly reminder that since you're viewing the website on a mobile device some of the styling may look a bit wonky. Chances are if it's really bad then I know about it and just haven't gotten to re-styling it for mobile yet. However, if it's a smaller detail or a suggestion you have then please let me know!")
+}
