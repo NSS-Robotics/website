0a240b23d833906fd342c247c2ec90e50520eac1
JacobK <techsupport@kreindler.ca>
2022-03-19 23:32:11 -0400

    feat: disabled reredict for `contact.html`

--- js/transition.js
+++ js/transition.js
@@ -1,6 +1,7 @@
 window.onload = () => {
     const transition_el = document.querySelector('.transition');
     const anchors = document.querySelectorAll('a');
+    const except = document.getElementById('except')
 
     setTimeout(() => {
         transition_el.classList.remove('is-active');
@@ -9,15 +10,20 @@ window.onload = () => {
     for(let i = 0; i < anchors.length; i++) {
         const anchor = anchors[i];
 
-        anchor.addEventListener('click', e => {
-            e.preventDefault();
-            let target = e.target.href;
+        if(anchor == except) {
+            continue;
+        }
+        else {
+            anchor.addEventListener('click', e => {
+                e.preventDefault();
+                let target = e.target.href;
 
-            transition_el.classList.add('is-active');
+                transition_el.classList.add('is-active');
 
-            setTimeout(() => {
-                window.location.href = target;
-            }, 500);
-        })
+                setTimeout(() => {
+                    window.location.href = target;
+                }, 500);
+            })
+        }
     }
 }
