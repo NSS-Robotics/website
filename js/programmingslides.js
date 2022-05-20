f093718dcc7d7c4a5cdac4219f787d5a1badcd6b
Jacob <techsupport@kreindler.ca>
2022-05-20 10:14:58 -0400

    feat: ran prettier

--- js/programmingslides.js
+++ js/programmingslides.js
@@ -2,16 +2,20 @@ var slideIndex = 1;
 programmingshowDivs(slideIndex);
 
 function programmingplusDivs(n) {
-  programmingshowDivs(slideIndex += n);
+  programmingshowDivs((slideIndex += n));
 }
 
 function programmingshowDivs(n) {
   var i;
   var x = document.getElementsByClassName("programmingSlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  x[slideIndex-1].style.display = "block";
+  x[slideIndex - 1].style.display = "block";
 }
