ee124ff989281297a66e3dfae058202a92c084a3
JacobK <techsupport@kreindler.ca>
2022-05-28 11:19:03 -0400

    del: removed mobile styling warning

--- js/mobilenotif.js
+++
@@ -1,5 +0,0 @@
-if (screen.width <= 480) {
-  alert(
-    "Hello! Just a frendly reminder that since you're viewing the website on a mobile device some of the styling may look a bit wonky. There shouldn't be too many issues but if you happen to find one please let us know!"
-  );
-}
