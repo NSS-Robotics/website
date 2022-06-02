bc647772ba8fd2a8f33ba32c5a70ae6c99f85fb7
Jacob <techsupport@kreindler.ca>
2022-06-02 15:57:14 -0400

    fix: tried to fix image flickering again

--- js/autobackground.js
+++ js/autobackground.js
@@ -28,3 +28,32 @@ function initBg() {
 }
 
 document.onload = initBg();
+
+function initload() {
+  const element = document.querySelector(".transition");
+  const header = document.querySelector(".header");
+  const images = [
+    "url(../imgs/headers/auto/1.webp",
+    "url(../imgs/headers/auto/2.webp",
+    "url(../imgs/headers/auto/3.webp",
+    "url(../imgs/headers/auto/4.webp",
+    "url(../imgs/headers/auto/5.webp",
+    "url(../imgs/headers/auto/6.webp",
+    "url(../imgs/headers/auto/7.webp",
+    "url(../imgs/headers/auto/8.webp",
+    "url(../imgs/headers/auto/9.webp",
+    "url(../imgs/headers/auto/10.webp",
+    "url(../imgs/headers/auto/11.webp",
+  ];
+
+  //In the span of 10ms set the backgroud image of the header to every one of the images in the array
+  for (let i = 0; i < images.length; i++) {
+    setTimeout(() => {
+      header.style.backgroundImage = images[i];
+      console.log(images[i]);
+    }, i * 10);
+  }
+  element.classList.remove("is-active");
+}
+
+window.onload = initload();
