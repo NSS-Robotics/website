882b1cf6269ed7f5966fafe3c0edc675c9eef4b6
JacobK <techsupport@kreindler.ca>
2022-06-03 22:21:15 -0400

    fix: tried to fix image flickering again

--- js/autobackground.js
+++ js/autobackground.js
@@ -1,17 +1,17 @@
 function changeBG() {
   const header = document.querySelector(".header");
   const images = [
-    "url(../imgs/headers/auto/1.webp",
-    "url(../imgs/headers/auto/2.webp",
-    "url(../imgs/headers/auto/3.webp",
-    "url(../imgs/headers/auto/4.webp",
-    "url(../imgs/headers/auto/5.webp",
-    "url(../imgs/headers/auto/6.webp",
-    "url(../imgs/headers/auto/7.webp",
-    "url(../imgs/headers/auto/8.webp",
-    "url(../imgs/headers/auto/9.webp",
-    "url(../imgs/headers/auto/10.webp",
-    "url(../imgs/headers/auto/11.webp",
+    "url(../imgs/headers/auto/1.webp)",
+    "url(../imgs/headers/auto/2.webp)",
+    "url(../imgs/headers/auto/3.webp)",
+    "url(../imgs/headers/auto/4.webp)",
+    "url(../imgs/headers/auto/5.webp)",
+    "url(../imgs/headers/auto/6.webp)",
+    "url(../imgs/headers/auto/7.webp)",
+    "url(../imgs/headers/auto/8.webp)",
+    "url(../imgs/headers/auto/9.webp)",
+    "url(../imgs/headers/auto/10.webp)",
+    "url(../imgs/headers/auto/11.webp)",
   ];
 
   const bg = images[Math.floor(Math.random() * images.length)];
@@ -29,31 +29,26 @@ function initBg() {
 
 document.onload = initBg();
 
-function initload() {
-  const element = document.querySelector(".transition");
-  const header = document.querySelector(".header");
-  const images = [
-    "url(../imgs/headers/auto/1.webp",
-    "url(../imgs/headers/auto/2.webp",
-    "url(../imgs/headers/auto/3.webp",
-    "url(../imgs/headers/auto/4.webp",
-    "url(../imgs/headers/auto/5.webp",
-    "url(../imgs/headers/auto/6.webp",
-    "url(../imgs/headers/auto/7.webp",
-    "url(../imgs/headers/auto/8.webp",
-    "url(../imgs/headers/auto/9.webp",
-    "url(../imgs/headers/auto/10.webp",
-    "url(../imgs/headers/auto/11.webp",
-  ];
-
-  //In the span of 10ms set the backgroud image of the header to every one of the images in the array
-  for (let i = 0; i < images.length; i++) {
-    setTimeout(() => {
-      header.style.backgroundImage = images[i];
-      console.log(images[i]);
-    }, i * 10);
+function preloadimages(arr) {
+  var newimages = [];
+  var arr = typeof arr != "object" ? [arr] : arr; //force arr parameter to always be an array
+  for (var i = 0; i < arr.length; i++) {
+    newimages[i] = new Image();
+    newimages[i].src = arr[i];
+    console.log("preloadimages: " + arr[i]);
   }
-  element.classList.remove("is-active");
 }
 
-window.onload = initload();
+preloadimages([
+  "../imgs/headers/auto/1.webp",
+  "../imgs/headers/auto/2.webp",
+  "../imgs/headers/auto/3.webp",
+  "../imgs/headers/auto/4.webp",
+  "../imgs/headers/auto/5.webp",
+  "../imgs/headers/auto/6.webp",
+  "../imgs/headers/auto/7.webp",
+  "../imgs/headers/auto/8.webp",
+  "../imgs/headers/auto/9.webp",
+  "../imgs/headers/auto/10.webp",
+  "../imgs/headers/auto/11.webp",
+]);
