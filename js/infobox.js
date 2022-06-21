8b2ad18bec9196133f01e5f8f6773eb9c76095ac
Jacob <techsupport@kreindler.ca>
2022-06-21 16:04:02 -0400

    feat: started working on name change info box

---
+++ js/infobox.js
@@ -0,0 +1,6 @@
+let btn = document.getelementById("goaway");
+
+function goaway() {
+  document.querySelector(".infobox").style.display = "none";
+  document.querySelector(".moreblack").style.display = "none";
+}
