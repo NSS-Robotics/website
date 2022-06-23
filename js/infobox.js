ed95f1c26ec5814ddb572f6be9074e08e07586b3
Jacob <techsupport@kreindler.ca>
2022-06-23 19:43:37 -0400

    feat: 4 days worth of colorizing

--- js/infobox.js
+++ js/infobox.js
@@ -1,6 +1,20 @@
-let btn = document.getelementById("goaway");
+let btn = document.querySelector("goaway");
+let moreblack = document.querySelector(".moreblack");
+let infobox = document.querySelector(".infobox");
 
 function goaway() {
-  document.querySelector(".infobox").style.display = "none";
-  document.querySelector(".moreblack").style.display = "none";
+  infobox.style.display = "none";
+  moreblack.style.display = "none";
+}
+
+// when a user loads the page for the first time the infobox is displayed but every time after that it is hidden
+if (localStorage.getItem("firsttime") === null) {
+  infobox.style.display = "block";
+  moreblack.style.display = "block";
+  localStorage.setItem("firsttime", "true");
+  console.log("first time");
+} else {
+  infobox.style.display = "none";
+  moreblack.style.display = "none";
+  console.log("not first time");
 }
