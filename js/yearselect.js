9919d0148560dbc644547b6daf6e615f2e5271f8
Jacob <techsupport@kreindler.ca>
2022-07-08 23:50:18 -0400

    feat: added fade animation to team change

--- js/yearselect.js
+++ js/yearselect.js
@@ -1,8 +1,16 @@
 document.getElementById("yearselector").addEventListener("change", function () {
   let newactive = document.querySelector("#" + this.value);
+  oldactive = document.querySelector(".people");
 
   removeClass();
   newactive.classList.add("showpeople");
+
+  oldactive.animate([{ opacity: "1" }, { opacity: ".5" }], {
+    duration: 1200,
+  });
+  newactive.animate([{ opacity: ".2" }, { opacity: "1" }], {
+    duration: 1200,
+  });
 });
 
 function removeClass() {
