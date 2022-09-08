d3dff0433fc3c600d04f0eea3f41979fbb2ef77a
Jacob <techsupport@kreindler.ca>
2022-09-08 19:00:42 -0400

    feat: astro rewrite init

---
+++ src/components/About/ImageSlider/index.jsx
@@ -0,0 +1,36 @@
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import './styles.css';
+
+// import required modules
+import { Mousewheel, Pagination } from 'swiper';
+
+export default function ImageSlider({ images }) {
+  return (
+    <>
+      <Swiper
+        direction={'vertical'}
+        slidesPerView={1}
+        spaceBetween={30}
+        mousewheel={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Mousewheel, Pagination]}
+        className="mySwiper"
+      >
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <img src={image} alt="Image" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
+}
