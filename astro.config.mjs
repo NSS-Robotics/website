f2ca24008a5a747f8fc290b6a7ff9e2eb004198d
Jacob <techsupport@kreindler.ca>
2022-09-09 09:04:20 -0400

    fix: went back to when everything worked

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from 'astro/config';
+
 import react from '@astrojs/react';
-import robotsTxt from 'astro-robots-txt';
-import { astroImageTools } from 'astro-imagetools';
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    integrations: true,
-  },
-  site: 'https://nssrobodevs.gq',
-  integrations: [react(), robotsTxt(), astroImageTools],
+  integrations: [react()],
 });
