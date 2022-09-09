7fa9fdd69a367c9ea2b1c11d79302063f8046ef7
JacobK <techsupport@kreindler.ca>
2022-09-08 22:59:23 -0400

    feat: add Vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@ import { defineConfig } from 'astro/config';
 import robotsTxt from 'astro-robots-txt';
 import react from '@astrojs/react';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    integrations: true,
+    integrations: true
   },
   site: 'https://nssrobodevs.gq',
   integrations: [react(), robotsTxt()],
+  output: "server",
+  adapter: vercel()
 });
