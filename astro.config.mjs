63d7ab4cc201c5d5b299130c400209d53beb61ee
JacobK <techsupport@kreindler.ca>
2022-09-08 20:14:13 -0400

    feat: added `robots.txt` integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-
+import robotsTxt from 'astro-robots-txt';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  experimental: {
+    integrations: true,
+  },
+  site: 'https://nssrobodevs.gq',
+  integrations: [react(), robotsTxt()],
 });
