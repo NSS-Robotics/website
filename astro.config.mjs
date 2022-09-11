7cdd0dd726754e6ec4fbad7f94f04d05b6357350
JacobK <techsupport@kreindler.ca>
2022-09-11 17:47:44 -0400

    feat: add sitemap generation on build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 import { defineConfig } from 'astro/config';
 import robotsTxt from 'astro-robots-txt';
-
 import react from '@astrojs/react';
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nssrobodevs.gq',
-
   // Important!
   // Only official '@astrojs/*' integrations are currently supported by Astro.
   // Add 'experimental.integrations: true' to make 'astro-robots-txt' working
   // with 'astro build' command.
   experimental: {
-    integrations: true,
+    integrations: true
   },
-  integrations: [react(), robotsTxt()],
+  integrations: [react(), robotsTxt(), sitemap()]
 });
