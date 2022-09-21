26555d92b8282af8b3c86b278c520a646c676651
JacobK <techsupport@kreindler.ca>
2022-09-20 22:55:26 -0400

    fix: changed css import order of donation slider

--- src/components/Sponsorship/Donating/index.jsx
+++ src/components/Sponsorship/Donating/index.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
+import './styles.css';
+
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-
-import './styles.css';
 
 // import required modules
 import { Pagination, Navigation } from 'swiper';
