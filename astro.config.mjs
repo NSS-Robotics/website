1c33e419b310df3ead070104791ac9f864c64a3b
Jacob <techsupport@kreindler.ca>
2023-08-31 13:13:56 -0400

    updated astro and removed `@astrojs/image`
    The image integration is now built into astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,23 @@
-import { defineConfig } from 'astro/config';
-import robotsTxt from 'astro-robots-txt';
-import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
-import { astroImageTools } from 'astro-imagetools';
-
-import image from '@astrojs/image';
+import { defineConfig } from "astro/config";
+import robotsTxt from "astro-robots-txt";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://knightowls.ca',
+  site: "https://knightowls.ca",
   vite: {
     ssr: {
-      external: ['svgo'],
+      external: ["svgo"],
     },
   },
   integrations: [
     react(),
     robotsTxt({
-      sitemap: 'https://knightowls.ca/sitemap-0.xml',
+      sitemap: "https://knightowls.ca/sitemap-0.xml",
     }),
     sitemap(),
-    image(),
     astroImageTools,
   ],
 });
