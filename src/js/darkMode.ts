03336ad697ef49c180eba3d618bd5ba15bc505fa
Goov314 <84781801+Goov314@users.noreply.github.com>
2024-12-23 12:58:02 -0500

    Added Switch
    Replaced button with switch and added dark mode transitions

--- src/js/darkMode.ts
+++ src/js/darkMode.ts
@@ -1,7 +1,7 @@
-const darkModeButton = document.getElementById("dark-mode-button");
+const darkModeSwitch = document.getElementById("dark-mode-button");
 
 // Listen for the dark mode toggle button click
-darkModeButton?.addEventListener("click", toggleDarkMode);
+darkModeSwitch?.addEventListener("click", toggleDarkMode);
 
 // Check the current theme on page load
 window.onload = checkDarkMode;
@@ -10,8 +10,10 @@ window.onload = checkDarkMode;
 document.addEventListener("astro:before-swap", (event) => {
   if (localStorage.getItem("dark-mode") === "true") {
     event.newDocument.querySelector("body")?.classList.add("dark-mode");
+    darkModeSwitch.checked = true
   } else {
     event.newDocument.querySelector("body")?.classList.remove("dark-mode");
+    darkModeSwitch.checked = false
   }
 });
 
@@ -19,8 +21,10 @@ document.addEventListener("astro:before-swap", (event) => {
 function checkDarkMode() {
   if (localStorage.getItem("dark-mode") === "true") {
     document.querySelector("body")?.classList.add("dark-mode");
+    darkModeSwitch.checked = true
   } else {
     document.querySelector("body")?.classList.remove("dark-mode");
+    darkModeSwitch.checked = false
   }
 }
 
@@ -29,6 +33,7 @@ function toggleDarkMode() {
   if (localStorage.getItem("dark-mode") === "true") {
     localStorage.setItem("dark-mode", "false");
     document.querySelector("body")?.classList.remove("dark-mode");
+    
   } else {
     localStorage.setItem("dark-mode", "true");
     document.querySelector("body")?.classList.add("dark-mode");
